1c73fe99950d37599df59e08c02774407b1eebf4
Mifaresss <9803211@gmail.com>
2022-09-06 13:32:38 +0300

    task complete

--- js/app.js
+++ js/app.js
@@ -1,45 +1,82 @@
-// Враги, которых наш игрок должен избегать
-var Enemy = function() {
-    // Здесь находятся переменные, применяемые к каждому из наших экземпляров,
-     // мы предоставили его для начала
+const enemyConfig = [
+   {y: 60, speedX: 410, },
+   {y: 145, speedX: 380, },
+   {y: 230, speedX: 340, },
+]
+class Enemies {
+   constructor(y, speedX) {
+      this.sprite = 'images/enemy-bug.png';
+      this.x = 0;
+      this.y = y;
+      this.speedX = speedX;
+   }
+   update(dt) {
+      this.x += this.speedX * dt;
+      if (this.x > 500) this.x = -100;
+      this.checkClash();
+   }
+   checkClash() {
+         if (player.x < this.x + 50 && player.x + 50 > this.x && player.y < this.y + 40 && player.y + 40 > this.y) {
+            player.x = player.startX;
+            player.y = player.startY;
+         }
+      }
+   render() {
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+   }
+}
+let enemy1 = new Enemies(enemyConfig[0].y, enemyConfig[0].speedX);
+let enemy2 = new Enemies(enemyConfig[1].y, enemyConfig[1].speedX);
+let enemy3 = new Enemies(enemyConfig[2].y, enemyConfig[2].speedX);
+const allEnemies = [enemy1, enemy2, enemy3]
 
-     // Изображение/спрайт для наших врагов, здесь используется
-     // помощник, который мы предоставили для простой загрузки изображений
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Обновить позицию противника, необходимый метод для игры 
-// Параметр: dt, временная дельта между тиками
-Enemy.prototype.update = function(dt) {
-    // Вы должны умножать любое движение на параметр dt, 
-    // что обеспечит одинаковую
-    // скорость игры для всех компьютеров.
-};
-
-// Нарисуйте врага на экране, необходимый метод для игры
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Теперь напишите свой собственный класс игрока
-// Этот класс требует update(), render() и
-// метод handleInput().
 
 
-// Теперь создайте экземпляры ваших объектов.
-// Поместите все вражеские объекты в массив с именем allEnemies
-// Поместите объект игрока в переменную с именем player
+class Player {
+   constructor(speedX, speedY) {
+      this.sprite = 'images/char-horn-girl.png';
+      this.startX = 200;
+      this.startY = 400;
+      this.x = 200;
+      this.y = 400;
+      this.speedX = speedX;
+      this.speedY = speedY;
+   }
+   update() {
+      if (player.y == -25) {
+         this.goToStart();
+         alert('Вы выиграли!')
+      }
+   }
+   render() {
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+   }
+   goToStart() {
+      this.y = this.startY;
+      this.x = this.startX;
+   }
+   handleInput(key) {
+      if (key === 'left') {
+         if (this.x > 0) this.x -= this.speedX;
+      } else if (key === 'up') {
+         if (this.y > 0) this.y -= this.speedY;
+      }
+      else if (key === 'right') {
+         if (this.x < 400) this.x += this.speedX;
+      } else if (key === 'down') {
+         if (this.y < 400) this.y += this.speedY;
+      }
+   }
+}
+const player = new Player(100, 85);
 
+document.addEventListener('keyup', function (e) {
+   var allowedKeys = {
+      37: 'left',
+      38: 'up',
+      39: 'right',
+      40: 'down'
+   };
 
-// Это слушает нажатия клавиш и отправляет клавиши на ваш
-// Метод Player.handleInput(). Вам не нужно изменять это.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-
-    player.handleInput(allowedKeys[e.keyCode]);
+   player.handleInput(allowedKeys[e.keyCode]);
 });
--- js/engine.js
+++ js/engine.js
@@ -1,185 +1,185 @@
 /* Engine.js
- * This file provides the game loop functionality (update entities and render),
- * draws the initial game board on the screen, and then calls the update and
- * render methods on your player and enemy objects (defined in your app.js).
- *
- * A game engine works by drawing the entire game screen over and over, kind of
- * like a flipbook you may have created as a kid. When your player moves across
- * the screen, it may look like just that image/character is moving or being
- * drawn but that is not the case. What's really happening is the entire "scene"
- * is being drawn over and over, presenting the illusion of animation.
- *
- * This engine makes the canvas' context (ctx) object globally available to make
- * writing app.js a little simpler to work with.
+ * Этот файл обеспечивает функциональность игрового цикла (обновление сущностей и рендеринг),
+  * рисует начальное игровое поле на экране, а затем вызывает обновление и
+  * методы рендеринга вашего игрока и вражеских объектов (определены в вашем app.js).
+  *
+  * Игровой движок работает, рисуя весь игровой экран снова и снова.
+  * как флипбук, который вы, возможно, создали в детстве. Когда ваш игрок перемещается
+  * на экране может показаться, что изображение/персонаж движется или движется
+  * нарисовано, но это не так. На самом деле происходит вся "сцена"
+  * рисуется снова и снова, создавая иллюзию анимации.
+  *
+  * Этот движок делает объект контекста холста (ctx) глобально доступным для создания
+  * Написание app.js немного упрощает работу.
  */
 
 var Engine = (function(global) {
-    /* Predefine the variables we'll be using within this scope,
-     * create the canvas element, grab the 2D context for that canvas
-     * set the canvas element's height/width and add it to the DOM.
-     */
-    var doc = global.document,
-        win = global.window,
-        canvas = doc.createElement('canvas'),
-        ctx = canvas.getContext('2d'),
-        lastTime;
+  /* Предварительно определите переменные, которые мы будем использовать в этой области,
+    * создайте элемент холста, возьмите 2D-контекст для этого холста
+    * установить высоту/ширину элемента холста и добавить его в DOM.
+   */
+  var doc = global.document,
+      win = global.window,
+      canvas = doc.createElement('canvas'),
+      ctx = canvas.getContext('2d'),
+      lastTime;
 
-    canvas.width = 505;
-    canvas.height = 606;
-    doc.body.appendChild(canvas);
+  canvas.width = 505;
+  canvas.height = 606;
+  doc.body.appendChild(canvas);
 
-    /* This function serves as the kickoff point for the game loop itself
-     * and handles properly calling the update and render methods.
-     */
-    function main() {
-        /* Get our time delta information which is required if your game
-         * requires smooth animation. Because everyone's computer processes
-         * instructions at different speeds we need a constant value that
-         * would be the same for everyone (regardless of how fast their
-         * computer is) - hurray time!
-         */
-        var now = Date.now(),
-            dt = (now - lastTime) / 1000.0;
+  /* Эта функция служит отправной точкой для самого игрового цикла
+    * и корректно обрабатывает вызовы методов update и render.
+    */
+  function main() {
+      /* Получите информацию о разнице во времени, которая требуется, если ваша игра
+        * требует плавной анимации. Потому что каждый компьютер обрабатывает
+        * инструкции на разных скоростях нам нужно постоянное значение, которое
+        * будет одинаковым для всех (независимо от того, насколько быстро их
+        * комп есть) - ура время!
+        */
+      var now = Date.now(),
+          dt = (now - lastTime) / 1000.0;
 
-        /* Call our update/render functions, pass along the time delta to
-         * our update function since it may be used for smooth animation.
-         */
-        update(dt);
-        render();
+      /* Вызываем наши функции обновления/рендеринга, передаем дельту времени в
+        * наша функция обновления, поскольку ее можно использовать для плавной анимации.
+        */
+      update(dt);
+      render();
 
-        /* Set our lastTime variable which is used to determine the time delta
-         * for the next time this function is called.
-         */
-        lastTime = now;
+      /* Устанавливаем нашу переменную lastTime, которая используется для определения дельты времени
+        * при следующем вызове этой функции.
+        */
+      lastTime = now;
 
-        /* Use the browser's requestAnimationFrame function to call this
-         * function again as soon as the browser is able to draw another frame.
-         */
-        win.requestAnimationFrame(main);
-    }
+      /* Используйте функцию браузера requestAnimationFrame для вызова этого
+        * работать снова, как только браузер сможет отрисовать другой кадр.
+        */
+      win.requestAnimationFrame(main);
+  }
 
-    /* This function does some initial setup that should only occur once,
-     * particularly setting the lastTime variable that is required for the
-     * game loop.
-     */
-    function init() {
-        reset();
-        lastTime = Date.now();
-        main();
-    }
+  /* Эта функция выполняет некоторую первоначальную настройку, которая должна произойти только один раз,
+    * в частности установка переменной lastTime, которая требуется для
+    * игровой цикл.
+    */
+  function init() {
+      reset();
+      lastTime = Date.now();
+      main();
+  }
 
-    /* This function is called by main (our game loop) and itself calls all
-     * of the functions which may need to update entity's data. Based on how
-     * you implement your collision detection (when two entities occupy the
-     * same space, for instance when your character should die), you may find
-     * the need to add an additional function call here. For now, we've left
-     * it commented out - you may or may not want to implement this
-     * functionality this way (you could just implement collision detection
-     * on the entities themselves within your app.js file).
-     */
-    function update(dt) {
-        updateEntities(dt);
-        // checkCollisions();
-    }
+  /* Эта функция вызывается main (наш игровой цикл) и сама вызывает все
+    * функций, которые могут потребоваться для обновления данных сущности. Основываясь на том, как
+    * вы реализуете обнаружение столкновений (когда два объекта занимают
+    * то же место, например, когда ваш персонаж должен умереть), вы можете найти
+    * необходимость добавить сюда дополнительный вызов функции. На данный момент мы оставили
+    * это закомментировано - вы можете или не хотите реализовывать это
+    * функциональность таким образом (вы можете просто реализовать обнаружение столкновений
+    * на самих объектах в вашем файле app.js).
+    */
+  function update(dt) {
+      updateEntities(dt);
+      // checkCollisions();
+  }
 
-    /* This is called by the update function and loops through all of the
-     * objects within your allEnemies array as defined in app.js and calls
-     * their update() methods. It will then call the update function for your
-     * player object. These update methods should focus purely on updating
-     * the data/properties related to the object. Do your drawing in your
-     * render methods.
-     */
-    function updateEntities(dt) {
-        allEnemies.forEach(function(enemy) {
-            enemy.update(dt);
-        });
-        player.update();
-    }
+  /* Это вызывается функцией обновления и перебирает все
+    * объекты в вашем массиве allEnemies, как определено в app.js и вызовах
+    * их методы update(). Затем он вызовет функцию обновления для вашего
+    * объект игрока. Эти методы обновления должны быть сосредоточены исключительно на обновлении
+    * данные/свойства, относящиеся к объекту. Сделайте свой рисунок в вашем
+    * Методы визуализации.
+    */
+  function updateEntities(dt) {
+      allEnemies.forEach(function(enemy) {
+          enemy.update(dt);
+      });
+      player.update();
+  }
 
-    /* This function initially draws the "game level", it will then call
-     * the renderEntities function. Remember, this function is called every
-     * game tick (or loop of the game engine) because that's how games work -
-     * they are flipbooks creating the illusion of animation but in reality
-     * they are just drawing the entire screen over and over.
-     */
-    function render() {
-        /* This array holds the relative URL to the image used
-         * for that particular row of the game level.
-         */
-        var rowImages = [
-                'images/water-block.png',   // Top row is water
-                'images/stone-block.png',   // Row 1 of 3 of stone
-                'images/stone-block.png',   // Row 2 of 3 of stone
-                'images/stone-block.png',   // Row 3 of 3 of stone
-                'images/grass-block.png',   // Row 1 of 2 of grass
-                'images/grass-block.png'    // Row 2 of 2 of grass
-            ],
-            numRows = 6,
-            numCols = 5,
-            row, col;
+  /* Эта функция сначала отрисовывает «уровень игры», затем вызывается
+    * Функция renderEntities. Помните, что эта функция вызывается каждый
+    * игровой тик (или цикл игрового движка), потому что так работают игры -
+    * это флипбуки, создающие иллюзию анимации, но на самом деле
+    * они просто рисуют весь экран снова и снова.
+    */
+  function render() {
+      /* This array holds the relative URL to the image used
+       * for that particular row of the game level.
+       */
+      var rowImages = [
+              'images/water-block.png',   // Top row is water
+              'images/stone-block.png',   // Row 1 of 3 of stone
+              'images/stone-block.png',   // Row 2 of 3 of stone
+              'images/stone-block.png',   // Row 3 of 3 of stone
+              'images/grass-block.png',   // Row 1 of 2 of grass
+              'images/grass-block.png'    // Row 2 of 2 of grass
+          ],
+          numRows = 6,
+          numCols = 5,
+          row, col;
 
-        // Before drawing, clear existing canvas
-        ctx.clearRect(0,0,canvas.width,canvas.height);
+      // Перед рисованием очищаем существующий холст
+      ctx.clearRect(0,0,canvas.width,canvas.height);
 
-        /* Loop through the number of rows and columns we've defined above
-         * and, using the rowImages array, draw the correct image for that
-         * portion of the "grid"
-         */
-        for (row = 0; row < numRows; row++) {
-            for (col = 0; col < numCols; col++) {
-                /* The drawImage function of the canvas' context element
-                 * requires 3 parameters: the image to draw, the x coordinate
-                 * to start drawing and the y coordinate to start drawing.
-                 * We're using our Resources helpers to refer to our images
-                 * so that we get the benefits of caching these images, since
-                 * we're using them over and over.
-                 */
-                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
-            }
-        }
+      /* Цикл по количеству строк и столбцов, которые мы определили выше
+        * и, используя массив rowImages, нарисуйте правильное изображение для этого
+        * часть "сетки"
+        */
+      for (row = 0; row < numRows; row++) {
+          for (col = 0; col < numCols; col++) {
+              /* Функция drawImage элемента контекста холста
+                * требует 3 параметра: изображение для рисования, координата x
+                *, чтобы начать рисовать, и координата y, чтобы начать рисовать.
+                * Мы используем помощников Resources для ссылок на наши изображения.
+                * так что мы получаем преимущества кэширования этих изображений, так как
+                * мы используем их снова и снова.
+                */
+              ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+          }
+      }
 
-        renderEntities();
-    }
+      renderEntities();
+  }
 
-    /* This function is called by the render function and is called on each game
-     * tick. Its purpose is to then call the render functions you have defined
-     * on your enemy and player entities within app.js
-     */
-    function renderEntities() {
-        /* Loop through all of the objects within the allEnemies array and call
-         * the render function you have defined.
-         */
-        allEnemies.forEach(function(enemy) {
-            enemy.render();
-        });
+  /* Эта функция вызывается функцией рендеринга и вызывается в каждой игре
+    * поставить галочку. Его цель состоит в том, чтобы затем вызвать функции рендеринга, которые вы определили.
+    * на ваших врагах и игроках в app.js
+    */
+  function renderEntities() {
+      /* Перебираем все объекты в массиве allEnemies и вызываем
+        * функция рендеринга, которую вы определили.
+        */
+      allEnemies.forEach(function(enemy) {
+          enemy.render();
+      });
 
-        player.render();
-    }
+      player.render();
+  }
 
-    /* This function does nothing but it could have been a good place to
-     * handle game reset states - maybe a new game menu or a game over screen
-     * those sorts of things. It's only called once by the init() method.
-     */
-    function reset() {
-        // noop
-    }
+  /* Эта функция ничего не делает, но могла бы быть хорошим местом для
+    * обрабатывать состояния сброса игры - может быть, новое игровое меню или экран окончания игры
+    * подобные вещи. Он вызывается только один раз методом init().
+    */
+  function reset() {
+      // noop
+  }
 
-    /* Go ahead and load all of the images we know we're going to need to
-     * draw our game level. Then set init as the callback method, so that when
-     * all of these images are properly loaded our game will start.
-     */
-    Resources.load([
-        'images/stone-block.png',
-        'images/water-block.png',
-        'images/grass-block.png',
-        'images/enemy-bug.png',
-        'images/char-boy.png'
-    ]);
-    Resources.onReady(init);
+  /* Идем дальше и загружаем все изображения, которые, как мы знаем, нам понадобятся
+    * Нарисуйте наш уровень игры. Затем установите init в качестве метода обратного вызова, чтобы при
+    * все эти изображения правильно загружены, наша игра запустится.
+    */
+  Resources.load([
+      'images/stone-block.png',
+      'images/water-block.png',
+      'images/grass-block.png',
+      'images/enemy-bug.png',
+      'images/char-horn-girl.png'
+  ]);
+  Resources.onReady(init);
 
-    /* Assign the canvas' context object to the global variable (the window
-     * object when run in a browser) so that developers can use it more easily
-     * from within their app.js files.
-     */
-    global.ctx = ctx;
+  /* Присвоить объект контекста холста глобальной переменной (окну
+    * объект при запуске в браузере), чтобы разработчикам было проще его использовать
+    * из своих файлов app.js.
+    */
+  global.ctx = ctx;
 })(this);
